16d546eff1e3f6e792b1b2b354820dd7c450f35f
algokelvin-373 <kelvinht373@gmail.com>
2023-04-22 10:32:29 +0700

    [Web Spring Boot] Web Develop

--- WebSpringBoot/WebHelloSpringBoot/src/main/java/com/algokelvin/web/controller/HelloController.java
+++ WebSpringBoot/WebHelloSpringBoot/src/main/java/com/algokelvin/web/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.algokelvin.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
@@ -9,6 +10,14 @@ public class HelloController {
     @GetMapping("/hellobasic")
     public String hello() {
         return "Hello, Spring Boot Version Basic";
+    }
+
+    @GetMapping("/hellowebhtml")
+    public ModelAndView hellowebhtml() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("hello"); // set view name
+        modelAndView.addObject("message", "<h1>Hello</h1>"); // add model attribute
+        return modelAndView;
     }
 
 }
